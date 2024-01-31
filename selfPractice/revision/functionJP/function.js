60e1e97c045f2e5d710a0b4ee0faed44ffe2b127
shubham <shubhampandeyup07@gmail.com>
2024-01-31 23:26:30 +0530

    function method

---
+++ selfPractice/revision/functionJP/function.js
@@ -0,0 +1,13 @@
+//function => a block of code that performs a particular task;
+
+function sayHello() {
+    console.log('Hey shubham')
+}
+sayHello();
+
+// parameters
+function add(a, b) {
+    console.log(a + b);
+}
+add(3, 5);  //arguments is here inside the call the function 
+add(34, 55);
