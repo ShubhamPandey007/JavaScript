320fddc3b03a5e474cd76b7fa1d60203ad9673a8
shubham <shubhampandeyup07@gmail.com>
2024-02-01 23:04:51 +0530

    use normal function to get output

---
+++ selfPractice/revision/functionJP/function1.js
@@ -0,0 +1,14 @@
+function multiply(a, b) {
+    let ans = a *b
+    return ans
+}
+
+let result = multiply(11,3);
+console.log(result);
+
+
+// 
+ function add(a, b) {
+    return a + b;
+ }
+ add(11, 3)
