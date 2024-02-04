891879447838bf090622bb84ca0b8a7f5ce37114
shubham <shubhampandeyup07@gmail.com>
2024-02-04 23:14:48 +0530

    here i can use while loop

---
+++ selfPractice/revision/loop/while.js
@@ -0,0 +1,11 @@
+// while Loop -> jab hame condition pata ho ki es particular  taksk ko achive karna hai, but ye nahi pata ki es task ko karne me kitana time lagega tab ham while loop ka use karte hai.
+
+let ip = 0;
+let house = 50;
+
+while ( ip != house ) {
+    ip = ip + 1;
+    console.log('step taken' + ip)
+}
+
+// while me ham pahle condition ko check karte hai uske bad code execute karte hai, but do while me vo phale code ko run karta hai uske bad condition ko check karta hai 
