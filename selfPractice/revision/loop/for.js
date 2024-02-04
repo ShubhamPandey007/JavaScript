20aa92c1f6deaee4c4c2d7595b538676216e718d
shubham <shubhampandeyup07@gmail.com>
2024-02-04 23:13:40 +0530

    this is for loop code

---
+++ selfPractice/revision/loop/for.js
@@ -0,0 +1,7 @@
+// loop ki help se hem kisi kam ko bar bar repeate kar sakte hai,
+
+//For Loop -> if you know how many times to loop is repeated.
+
+for ( let i = 1; i <= 10; i++) {
+    console.log("shubham" + i)                // for loop tab use karna hai jab hame exact pata ho ki loop kitani bar chalana hai
+}
