eb36837008f8c2ab86c87cd2d43bdfb19c6cdd72
shubham <shubhampandeyup07@gmail.com>
2024-02-04 23:15:30 +0530

    this is dowhile loop

---
+++ selfPractice/revision/loop/doWhile.js
@@ -0,0 +1,12 @@
+// while me ham pahle condition ko check karte hai uske bad code execute karte hai, but do while-> me vo phale code ko run karta hai uske bad condition ko check karta hai 
+
+
+let ip = 50;
+let house = 50;
+
+do {
+    ip = ip + 1;
+    console.log('Step taken' + ip);
+} while (ip <= house);
+
+
